5b146562c7da35daf42bd5a201042dec7c0e4e3f
Roman Osadchyi <romanos4740@gmail.com>
2023-11-09 20:33:34 +0200

    some message

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.demofx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.demofx to javafx.fxml;
+    exports com.example.demofx;
+}
